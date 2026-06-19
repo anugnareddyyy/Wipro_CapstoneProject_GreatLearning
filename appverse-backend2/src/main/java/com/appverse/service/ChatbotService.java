bdb53f6ad8c50236f2ec30c573395f26f4d655aa
anugnareddyyy <anugunareddy@gmail.com>
2026-06-19 11:56:45 +0530

    Delete appverse-backend2 directory

--- appverse-backend2/src/main/java/com/appverse/service/ChatbotService.java
+++
@@ -1,8 +0,0 @@
-package com.appverse.service;
- 
-import com.appverse.dto.request.ChatRequest;
-import com.appverse.dto.response.ChatResponse;
- 
-public interface ChatbotService {
-    ChatResponse chat(ChatRequest request, Long userId);
-}
